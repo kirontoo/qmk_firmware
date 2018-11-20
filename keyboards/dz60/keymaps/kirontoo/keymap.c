d18abd96f57e70077659676e49390b431ce51343
Amy Dang <kirontoo@gmail.com>
2018-11-20 00:25:00 -0800

    called wrong function for right shift in _BL

--- keyboards/dz60/keymaps/kirontoo/keymap.c
+++ keyboards/dz60/keymaps/kirontoo/keymap.c
@@ -17,6 +17,7 @@
 #define SHUT_DWN LALT(KC_F4)    // alt+f4
 #define LBRCK LSFT(KC_LBRC)   // left bracket {
 #define RBRCK LSFT(KC_RBRC)   // right bracket }
+#define RSFT_Q RSFT_T(KC_SLSH)   // right shift on hold, / on tap
 
 /* LAYOUT_60_b_ansi (maximized DZ60 Plate B layout for ANSI)
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,          KC_1,    KC_2,     KC_3,    KC_4,    KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,           KC_MINS,   KC_EQL,      KC_BSPC,   KC_PSCR,
         KC_TAB,          KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,           KC_LBRC,   KC_RBRC,     KC_BSLS,
         LT(_FL, KC_ESC), KC_A,    KC_S,     KC_D,    KC_F,    KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,        KC_QUOT,   KC_ENT,
-        KC_LSFT,         KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   RSFT(KC_SLSH),  KC_UP,     TG(_NUMPAD),
+        KC_LSFT,         KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   RSFT_Q,         KC_UP,     TG(_NUMPAD),
         KC_LCTL,         KC_LGUI, KC_LALT,  KC_SPC,  MO(_FL), KC_BSPC,  MO(_KL),  TT(_ML),  KC_LEFT,  KC_DOWN,  KC_RIGHT
     ),
 
