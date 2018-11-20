4b31fec84668f142041f6cdeca3dc3e215c489f9
Amy Dang <kirontoo@gmail.com>
2018-11-19 23:39:03 -0800

    add config and rules settings for keyboard

---
+++ keyboards/dz60/keymaps/kirontoo_60_b_ansi/config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x2260
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    KBDFans
+#define PRODUCT         DZ60
+#define DESCRIPTION     DZ60 Keyboard
+
+// Number of TAPs to toggle layer
+#define TAPPING_TOGGLE 3
+
+// RGB Settings
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_SLEEP        // RGB lighting switched off when host goes to sleep
+#endif
