3e2ebc692f146e1f452df5cf830d71b19942596b
Amy Dang <kirontoo@gmail.com>
2019-02-17 12:46:22 -0800

    implement Tap dance functionality

---
+++ keyboards/laplace/keymaps/kirontoo/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config.h"
+
+#define TAPPING_TERM 200
+
+#endif
--- keyboards/laplace/keymaps/kirontoo/keymap.c
+++ keyboards/laplace/keymaps/kirontoo/keymap.c
@@ -1,26 +1,38 @@
 #include QMK_KEYBOARD_H
 
+// Layers
 #define _QWERTY 0
 #define _COLEMAK 1
-#define _FN1 2
-#define _FN2 3
+#define _LOWER 2
+#define _RAISE 3
+#define _FN1
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
 #define FN1 MO(_FN1)
-#define FN2 MO(_FN2)
 #define SP_LWR LT(_LOWER, KC_SPACE)
 #define BS_RAS LT(_RAISE, KC_BSPC)
-#define TSK_MNG LCTL(LSFT(KC_ESC))   // ctrl+shift+esc
-#define LCK_SCRN LGUI(KC_L)     // win+L
+
+// shortcuts
+#define TSK_MNG LCTL(LSFT(KC_ESC))
+#define LCK_SCRN LGUI(KC_L)
+#define DSKTOP LGUI(KC_B)
+#define PSCRN LALT(KC_PSCR)
 
 enum custom_keycodes {
-                      COLEMAK = SAFE_RANGE,
-                      QWERTY,
-                      PSRCN,
-                      DSKTOP,
-                      EXT_APP
+  COLEMAK = SAFE_RANGE,
+  QWERTY
+};
+
+// Tap Dance
+enum {
+  TD_F4 = 0
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  // tap once for F4, twice for ALT+F4
+  [TD_F4] = ACTION_TAP_DANCE_DOUBLE(KC_F4, LALT(KC_F4));
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -29,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,
     KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_QUOT,  KC_ENT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-    KC_LCTL,  KC_LALT,  KC_LGUI,  FN1,      SP_LWR,   BS_RAS,   KC_RGUI,  KC_RALT,  FN2,      KC_RCTL
+    KC_LCTL,  KC_LALT,  KC_LGUI,  FN1,      SP_LWR,   BS_RAS,   KC_RGUI,  KC_RALT,  KC_MENU,  KC_RCTL
   ),
 
   [_QWERTY] = LAYOUT(
-    KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_G,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,     KC_LBRC,  KC_RBRC,
-    KC_ESC,   KC_A,     KC_R,     KC_S,     KC_T,     KC_D,     KC_H,     KC_N,     KC_E,     KC_I,     KC_O,  KC_ENT,
+    KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_G,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,
+    KC_ESC,   KC_A,     KC_R,     KC_S,     KC_T,     KC_D,     KC_H,     KC_N,     KC_E,     KC_I,     KC_O,     KC_ENT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_K,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-    KC_LCTL,  KC_LALT,  KC_LGUI,  FN1,      SP_LWR,   BS_RAS,   KC_RGUI,  KC_RALT,  FN2,      KC_RCTL
+    KC_LCTL,  KC_LALT,  KC_LGUI,  FN1,      SP_LWR,   BS_RAS,   KC_RGUI,  KC_RALT,  KC_MENU,  KC_RCTL
   ),
 
 
@@ -55,10 +67,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN1] = LAYOUT(
-    PSCRN,  KC_F1,  KC_F2,    KC_F3,  EXT_APP,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  QWERTY,
-    TSK_MNG,  KC_F5,  KC_F6,    KC_F7,  KC_F8,   _______,  _______,  _______,  _______,  _______,  _______,  COLEMAK,
-    DSKTOP,  KC_F9,  KC_10,    KC_F11, KC_12,   _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_MPLY,
-    LCK_SCRN,  _______,  _______,  _______,  _______,   _______, KC_MPRV,  KC_VOLD,  KC_VOLU,  KC_MNXT
+    PSCRN,     KC_F1,    KC_F2,    KC_F3,    TD(TD_F4), _______,  _______,  _______,  _______,  _______,  _______,  _______,  QWERTY,
+    TSK_MNG,   KC_F5,    KC_F6,    KC_F7,    KC_F8,     _______,  _______,  _______,  _______,  _______,  _______,  COLEMAK,
+    DSKTOP,    KC_F9,    KC_10,    KC_F11,   KC_12,     _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_MPLY,
+    LCK_SCRN,  _______,  _______,  _______,  _______,   _______,  KC_MPRV,  KC_VOLD,  KC_VOLU,  KC_MNXT
 )
 };
 
@@ -79,24 +91,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 
     return false;
-    break;
-
-  case PSCRN:
-    if (record->event.pressed) {
-      SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_PSCREEN) SS_UP(X_ALT));
-    }
-    break;
-
-  case DSKTOP:
-    if (record->event.pressed) {
-      SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_D) SS_UP(X_LGUI));
-    }
-    break;
-
-  case EXT_APP:
-    if (record->event.pressed) {
-      ACTION_TAP_DANCE_DOUBLE(KC_F4, LALT(KC_F4));
-    }
     break;
 
   default:
